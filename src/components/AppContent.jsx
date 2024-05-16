bc76353768c9fe81ac2885eb0c60b1a87f523450
Alex <merkulovwork@mail.ru>
2024-05-16 12:57:00 +1000

    начал писать логику

--- src/components/AppContent.jsx
+++ src/components/AppContent.jsx
@@ -1,6 +1,13 @@
 import {Content} from "antd/es/layout/layout";
+import {useCrypto} from "../context/cryptoContext";
+import {Spin} from "antd";
 
 export default function AppContent(){
+    const {loading, assets, crypto} = useCrypto()
+
+    if (loading) {
+        return <Spin fullscreen/>
+    }
 
     return(
         <Content>
--- src/components/AppSider.jsx
+++ src/components/AppSider.jsx
@@ -1,10 +1,42 @@
 import Sider from "antd/es/layout/Sider";
+import {List} from "antd";
+import styled from "styled-components";
+import {useState} from "react";
 
 export default function AppSider(){
+    const data = [
+        'Мой портфель',
+        'Рынок'
+    ]
+
+    const CryptoLogo = styled.div`
+      height: 64px;
+      font-weight: bold;
+      font-size: 28px;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    `
+
+    const SectionClicker = styled.button`
+      border: none;
+      
+    `
+
+    const [s, setS] = useState(1)
 
     return (
         <Sider theme={"light"}>
-            eeee
+            <CryptoLogo >
+                My Crypto {s}
+            </CryptoLogo>
+            <List
+                dataSource={data}
+                renderItem={(item) => <List.Item onClick={()=>setS(0)}>{item}</List.Item>}
+            >
+            </List>
+
         </Sider>
     )
 }
